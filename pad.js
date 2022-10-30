7e247513f58300b41a4ae3cbf8932b6f70fd777b
Nyves <68265244+Nyves0217@users.noreply.github.com>
2022-10-30 10:54:23 +0545

    Create pad.js

---
+++ pad.js
@@ -0,0 +1,35 @@
+export default class InputHandler {
+  constructor(pad, game) {
+    document.addEventListener("keydown", event => {
+      switch (event.keyCode) {
+        case 37:
+          pad.moveLeft();
+          break;
+
+        case 39:
+          pad.moveRight();
+          break;
+
+        case 27:
+          game.togglePause();
+          break;
+
+        case 32:
+          game.start();
+          break;
+      }
+    });
+
+    document.addEventListener("keyup", event => {
+      switch (event.keyCode) {
+        case 37:
+          if (pad.speed < 0) pad.stop();
+          break;
+
+        case 39:
+          if (pad.speed > 0) pad.stop();
+          break;
+      }
+    });
+  }
+}
